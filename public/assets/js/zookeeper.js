3e7508c82a858d847a91fd1dca4b293e91ba696a
Rick Barendse <dazedeyes@yahoo.com>
2022-04-08 12:06:01 -0700

    zookeeper functionality

--- public/assets/js/zookeeper.js
+++ public/assets/js/zookeeper.js
@@ -1,3 +1,4 @@
+const $zookeeperForm = document.querySelector("#zookeeper-form");
 const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
@@ -20,18 +21,45 @@ const printResults = resultArr => {
   $displayArea.innerHTML = animalHTML.join('');
 };
 
-const getZookeepers = () => {
-  fetch('/api/zookeepers')
+const getZookeepers = (formData ={}) => {
+  let queryUrl ='/api/zookeepers?';
+
+  Object.entries(formData).forEach(([key, value]) => {
+    queryUrl += `${key}=${value}&`;
+  });
+  
+  fetch(queryUrl)
     .then(response => {
-      if (!response.ok) {
-        return alert('Error: ' + response.statusText);
+      if(!response.ok) {
+        return alert(`Error: ${response.statusText}`);
       }
       return response.json();
     })
+  // fetch('/api/zookeepers')
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       return alert('Error: ' + response.statusText);
+  //     }
+  //     return response.json();
+  //   })
     .then(zookeeperArr => {
       console.log(zookeeperArr);
       printResults(zookeeperArr);
     });
 };
 
+const handleGetZookeepersSubmit = event => {
+  event.preventDefault();
+  const nameHTML = $zookeeperForm.querySelector('[name="name"]');
+  const name = nameHTML.value;
+
+  const ageHTML = $zookeeperForm.querySelector('[name="age"]');
+  const age = ageHTML.value;
+
+  const zookeepersObject = { name, age };
+
+  getZookeepers(zookeepersObject);
+};
+
+$zookeeperForm.addEventListener('submit', handleGetZookeepersSubmit);
 getZookeepers();
